b375c7446a8c2dfdce74255edf0db1164d835a3f
pavvelpotapov <pavelminsk1979@mail.ru>
2025-07-19 15:57:52 +0300

    comit1

---
+++ src/app/rote1/page.tsx
@@ -0,0 +1,11 @@
+import {Menu} from "@/components/Menu";
+
+export default function Route1() {
+    return (
+        <div>
+            <Menu/>
+            <h2 className="text-4xl font-bold"> эта роут один(1)</h2>
+
+        </div>
+    );
+}
---
+++ src/components/Menu.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export const Menu = () => {
+    return (
+        <div>
+            <Link href={'/'}>Главная-----</Link>
+            <Link href={'/rote1'}>----Путь1-СТРАНИЦА1</Link>
+        </div>
+    )
+}
